4d14bb16f591e519968da1106ab2e8c10a62eb29
shamsudheen kv <kvshamsudheen@gmail.com>
2021-07-09 12:29:22 -0400

    Update load.homologs.r

--- R/load.homologs.r
+++ R/load.homologs.r
@@ -16,7 +16,8 @@ load.homologs <- function(){
     hom_vert = hom_vert[-1,]
 
     # The table is badly formatted, so drop rubbish
-    hom_vert = suppressWarnings(hom_vert[!is.na(as.numeric(hom_vert$`HomoloGene ID`)),])
+    # The table format has changed
+    hom_vert = suppressWarnings(hom_vert[!is.na(as.numeric(hom_vert$`DB Class Key`)),])
 
     # Provide some output to help the user
     print("Species for which homology classes are available:")
